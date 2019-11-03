d54b73fb12fd412198d8ce004bc594f19475498b
Jorge Canchon <jorgecanchon@gmail.com>
2019-11-03 12:02:54 -0500

    Primera version chat

---
+++ src/config.js
@@ -0,0 +1,19 @@
+export default {
+    apiKey: "AIzaSyDvp7udlKeCYEx0-rbmR7LU1sxbQ3jcNqM",
+    authDomain: "chatmotordoc.firebaseapp.com",
+    databaseURL: "https://chatmotordoc.firebaseio.com",
+    projectId: "chatmotordoc",
+    storageBucket: "chatmotordoc.appspot.com",
+    messagingSenderId: "647530353804",
+    appId: "1:647530353804:web:74d377cacdb63cf8e7d7a9",
+    measurementId: "G-YBGHHFL2P2"
+  };
+
+//   export default {
+//     apiKey: "AIzaSyAaODb7bCoZPvV4gdVyG_sV_Lc1_GuVdwg",
+//     authDomain: "react-intro-37cd1.firebaseapp.com",
+//     databaseURL: "https://react-intro-37cd1.firebaseio.com",
+//     projectId: "react-intro-37cd1",
+//     storageBucket: "react-intro-37cd1.appspot.com",
+//     messagingSenderId: "181293593583"
+//   };
